b4c21793b9429a173c8741e7d7529e16852f5d61
Seleem Aleshinloye <seleemmiah1@gmail.com>
2023-03-25 22:22:32 +0100

    This is casual cal projects

---
+++ github/Calculator.py
@@ -0,0 +1,31 @@
+while True:
+        print("1 Addition")
+        print("2 Subtraction")
+        print("3 Multiplication")
+        print("4 Division")
+        print()
+        print("Enter g or G to exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice =='g' or choice == 'G':
+            break
+
+        num1 = float(input("Enter number 1 : "))
+        num2 = float(input("Enter number 2 : "))
+
+        if choice == "1":
+            print(num1, "+", num2, "=", (num1+num2))
+        elif choice == "2":
+            print(num1, "-", num2, "=", (num1-num2))
+        elif choice == "3":
+            print(num1, "*", num2, "=", (num1*num2))
+        elif choice == "4":
+            if num2 ==0.0:
+                print("Divide by 0 Error")
+            else:
+                print(num1, "/", num2, "=",(num1/num2))
+        else:
+            print("invalid choice")
+
+        print()
